f0ade10c22adee5a8488731dfa5a01b85db69d98
Jessica Nelson <yahwehschica@yahoo.com>
2021-10-09 12:28:12 -0400

    commit tree 601f357f0522b72b6bb93b47c8ede85262a162f2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const newDrivers = drivers.filter(names => names.toLowerCase()
+    === name.toLowerCase());
+    return newDrivers;
+};
+
+function fuzzyMatch(array, name) {
+    return array.filter(el => el.slice(0,1) === name.slice(0,1))
+}
+
+
+function matchName(driverArray, string) {
+    return driverArray.filter(element => element.name === string);
+}
+        
+
+
+
+
+//   `matchName` - This function takes an array of `driver` objects and a `string`
+//   as arguments. Each `driver` object has two properties: `name` and `hometown`.
+//   The function should return each element whose `name` property matches the
+//   provided `string` argument.
+
+
+// function isBigEnough(value) {
+//     return value >= 10
+//   }
